bc6d893c5e812bbe61df6cb3481263b5cc447617
ElGolondras <jfoncla2@gmail.com>
2026-01-27 16:25:52 +0100

    Migrations

---
+++ src/database/migrations/2026_01_27_151430_equip-aula.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('aulas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->timestamp('batch');
+        });
+
+        Schema::create('dispositivos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('tipo');
+            $table->string('modelo');
+            $table->string('estado');
+            $table->foreignId('aula_id')->index();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('aulas');
+        Schema::dropIfExists('dispositivos');
+    }
+};
